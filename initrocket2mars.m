27497f38c44534729d48afb790596368bc5998e9
louisg0709@ <louisg0709@gmail.com>
2018-02-13 17:04:00 +0000

    added some help and fixed positions

--- initrocket2mars.m
+++ initrocket2mars.m
@@ -32,7 +32,7 @@ ys=149.6e9; %y position of sun
 
 
 xma=0; %xpos mars
-yma=225e9; %ypos mars
+yma=225e9+ys; %ypos mars (note added sun distance on)
 vxma=-24130;
 vyma=0;
 
@@ -70,32 +70,6 @@ theta=theta*2*pi/360;
 
 vx=u*cos(theta);
 vy=u*sin(theta);
-
-
-figure;
-hold on;
-re=re/2; %modification for moon model
-
-hax1=line([-re-box -re-box], [re+box -re-box]);
-hax2=line([re+box -re-box], [re+box +re+box]);
-hax3=line([re+box re+box], [-re-box +re+box]);
-hax4=line([-re-box re+box], [-re-box -re-box]);
-
-  set( hax1 , 'Color' ,'red' );
-plot( xe , ye ,'o','MarkerFaceColor','r','MarkerSize',30);
-hold on;
-h=plot( x , y ,'o','MarkerFaceColor','g','MarkerSize',10);
-hold on;
-hm=plot( xm , ym ,'o','MarkerFaceColor','b','MarkerSize',10);
-hold on;
-plot( xma , yma ,'o','MarkerFaceColor','r','MarkerSize',30);
-hold on;
-
-plot( xs , ys ,'o','MarkerFaceColor','r','MarkerSize',90);
-hold on;
-
-
-%output
 
 
 
